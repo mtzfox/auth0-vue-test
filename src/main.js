5f75dfeaa062fca9b6c84fc9e6f85cd072eb6f39
Mike <21209421+mtzfox@users.noreply.github.com>
2022-11-30 13:14:26 -0800

    Setup build for vue auth app

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createAuth0 } from '@auth0/auth0-vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import './style.css'
+
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-nq6cgd3w773e7fxn.us.auth0.com",
+    client_id: "R9RYBjEMEB4EzsZJj4vDETUkc9XDp2Wj",
+    redirect_uri: window.location.origin,
+  })
+);
+
+app.mount('#app');
